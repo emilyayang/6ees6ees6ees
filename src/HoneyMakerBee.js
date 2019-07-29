4934d6e8ecb9ceded28f949c298416cb8bc8cea6
Emily Yang <emilyyang@Emilys-MacBook-Air.local>
2019-07-29 12:30:02 -0700

    finished 6ees

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,38 @@
-class HoneyMakerBee {
+class HoneyMakerBee extends Bee {
   // TODO..
+  constructor() {
+    super()
+  	this.age = 10;
+	  this.job = "make honey";
+	  this.honeyPot = 0;
+  }
+  makeHoney () {
+	this.honeyPot++;
+ }
+
+  giveHoney () {
+	this.honeyPot--;
+ }
 };
+
+
+// var HoneyMakerBee = function(color, food) {
+// 	Bee.call(this, color);
+// 	Grub.call(this, food);
+// 	this.age = 10;
+// 	this.job = "make honey";
+// 	this.color = "yellow";
+// 	this.honeyPot = 0;
+
+// };
+
+//  HoneyMakerBee.prototype = Object.create(Grub.prototype);// re-define child prototype to Parent prototype
+//  HoneyMakerBee.prototype.constructor = HoneyMakerBee;// return original constructor to Child
+
+//   HoneyMakerBee.prototype.makeHoney = function() {
+// 	this.honeyPot++;
+//  }
+
+//   HoneyMakerBee.prototype.giveHoney = function() {
+// 	this.honeyPot--;
+//  }
--- src/RetiredForagerBee.js
+++ src/RetiredForagerBee.js
@@ -1,3 +1,54 @@
-class RetiredForagerBee {
-  // TODO..
+class RetiredForagerBee extends ForagerBee{
+  constructor(){
+    super()
+    this.age = 40
+    this.job = 'gamble';
+    this.canFly = false;
+    this.color = 'grey';
+  }
+  forage(){
+    return 'I am too old, let me play cards instead';
+  }
+  gamble(treasure){
+    this.treasureChest.push(treasure);
+  }
 };
+
+
+// var RetiredForagerBee = function(treasureChest, food) {
+// 	ForagerBee.call(this, treasureChest)
+// 	Grub.call(this, food);
+
+// 	this.age = 40;
+// 	this.job = "gamble";
+// 	this.color = "grey";
+// 	this.canFly = false;
+//     // this.treasureChest = [];
+// };
+
+//  RetiredForagerBee.prototype = Object.create(Grub.prototype);// re-define child prototype to Parent prototype
+//  RetiredForagerBee.prototype.constructor = RetiredForagerBee;// return original constructor to Child
+
+
+// RetiredForagerBee.prototype.forage = function(treasure) {
+//     return "I am too old, let me play cards instead";
+// }
+
+// RetiredForagerBee.prototype.gamble = function(treasure) {
+//     this.treasureChest.push(treasure);
+// };
+
+//  // Create a RetiredForagerBee class, in pseudoclassical style, with:
+
+//  // call the ForagerBee superclass
+//  // set the prototype
+//  // set the constructor
+//  // an age property that is set to 40
+//  // a job property that is set to gamble
+//  // a canFly property that is set to false
+//  // a color property that is set to grey
+//  // a forage method that returns "I am too old, let me play cards instead"
+//  // a food property that is inherited from grub
+//  // an eat method that is inherited from grub
+//  // a treasureChest property inherited from ForagerBee that is set to an empty array []
+//  // an always winning gamble method that allows the bee to add a treasure to the treasureChest
